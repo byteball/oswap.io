125a4db94a59b1676285fb00721c617c9b772c61
kaiurin <43986607+kaiurin@users.noreply.github.com>
2023-08-16 09:34:39 +0300

    fixed tooltips on mobile devices (#16)

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,45 @@
+<template>
+  <VTooltip>
+    <div style="font-size: 20px" class="d-block">
+      <TooltipIcon />
+    </div>
+    <template #popper>
+      <div>
+        {{ tooltipDescription }}
+      </div>
+    </template>
+  </VTooltip>
+</template>
+
+<script>
+import TooltipIcon from '@/components/TooltipIcon.vue';
+
+export default {
+  components: { TooltipIcon },
+  props: ['tooltipDescription']
+};
+</script>
+
+<style>
+.v-popper--theme-tooltip {
+  font: 16px 'Calibre-Medium', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: subpixel-antialiased;
+  line-height: 1.5;
+  word-wrap: break-word;
+  white-space: normal;
+  max-width: 250px;
+}
+
+.v-popper--theme-tooltip .v-popper__inner {
+  background: #1c2024;
+}
+
+.v-popper--theme-tooltip .v-popper__arrow-inner {
+  visibility: visible;
+  border-color: #1c2024;
+}
+
+.v-popper--theme-tooltip .v-popper__arrow-outer {
+  border-color: #1c2024;
+}
+</style>
