c98afcfa6d9ae34651f110bb22e4e2a6ddafef6f
Evgenii Stulnikov <xxjenekxx@gmail.com>
2025-07-04 01:43:05 +0300

    added link to GetMeIn (#19)
    * added link to GetMeIn
    
    * removed unnecessary query param for GetMeIn

--- src/components/LabelBalance.vue
+++ src/components/LabelBalance.vue
@@ -3,7 +3,15 @@
     (max
     <a class="color-gray-6" @click="select"> <Amount :asset="decimals_asset || asset" :value="balance" /> </a>)
   </span>
+  <span v-else-if="!balance && this.asset === 'base'">
+    (get GBYTEs at
+    <a :href="getLinkToGetMeIn" target="_blank"
+      >GetMeIn<Icon name="external-link" class="ml-1" size="18"/></a
+    >)
+  </span>
 </template>
+
+
 
 <script>
 import { getBalance } from '@/helpers/_oswap';
@@ -14,6 +22,16 @@ export default {
     balance() {
       const { balances } = this.auth;
       return this.asset ? getBalance(balances, this.asset) : 0;
+    },
+    getLinkToGetMeIn() {
+      const { address } = this.auth;
+      let link = 'https://getmein.ooo/';
+
+      if (address) {
+        link += `?recipient=${address}`;
+      }
+
+      return link;
     }
   },
   methods: {
