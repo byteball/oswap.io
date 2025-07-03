cf5ad16fa290460d71df0e19540c9218836b6fa4
Evgenii Stulnikov <xxjenekxx@gmail.com>
2025-07-04 01:20:46 +0300

    added link to GetMeIn

--- src/components/LabelBalance.vue
+++ src/components/LabelBalance.vue
@@ -3,6 +3,12 @@
     (max
     <a class="color-gray-6" @click="select"> <Amount :asset="decimals_asset || asset" :value="balance" /> </a>)
   </span>
+  <span v-else-if="!balance && this.asset === 'base'">
+    (get GBYTEs at
+    <a :href="getLinkToGetMeIn" target="_blank"
+      >GetMeIn<Icon name="external-link" class="ml-1" size="18"/></a
+    >)
+  </span>
 </template>
 
 <script>
@@ -14,6 +20,17 @@ export default {
     balance() {
       const { balances } = this.auth;
       return this.asset ? getBalance(balances, this.asset) : 0;
+    },
+    getLinkToGetMeIn() {
+      const { address } = this.auth;
+      let link = 'https://getmein.ooo/';
+      if (address) {
+        link += `?recipient=${address}&dst_network=Obyte`;
+      } else {
+        link += '?dst_network=Obyte';
+      }
+
+      return link;
     }
   },
   methods: {
